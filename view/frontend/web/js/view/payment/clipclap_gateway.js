3f334b69e731a56c6794e1a1fb1d66f90d1c6a70
Jairo Mejia <jairo@codeiseverywhere.com>
2016-03-02 21:27:49 -0500

    update

--- view/frontend/web/js/view/payment/clipclap_gateway.js
+++ view/frontend/web/js/view/payment/clipclap_gateway.js
@@ -21,7 +21,25 @@ define(
             }
         );
         /** Add view logic here if needed */
-        
+
         return Component.extend({});
     }
 );
+
+var _$clipclap = _$clipclap || {};
+_$clipclap._setKey = 'Vc7Jhi1v0DC9Tq0n6Ln5';
+_$clipclap._themeButton = "blue";
+_$clipclap._Buttons = {
+    "#botonClipClap":{
+        'paymentRef': 'ref0000001',
+        'netValue': '13000',
+        'taxValue': '1000',
+        'tipValue': '500',
+        'description': 'Combo 1. Hambuerguesa, Perro y Gaseosa'
+    }
+};
+(function() {
+    var cc = document.createElement('script'); cc.type = 'text/javascript'; cc.async = true;
+    cc.src = 'https://clipclap.co/paybutton/js/paybutton.min.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(cc, s);
+})();
