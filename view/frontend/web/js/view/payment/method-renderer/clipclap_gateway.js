971c46ff8bbb7561816d28c36e0e75cec5826773
Jairo Mejia <jairo@codeiseverywhere.com>
2016-03-08 10:05:54 -0500

    update

--- view/frontend/web/js/view/payment/method-renderer/clipclap_gateway.js
+++ view/frontend/web/js/view/payment/method-renderer/clipclap_gateway.js
@@ -3,13 +3,15 @@
  * See COPYING.txt for license details.
  */
 /*browser:true*/
-
-        console.log(window._$clipclap);
-        // console.log('call button file');
-        var cc = document.createElement('script'); cc.type = 'text/javascript'; cc.async = true;
-        cc.src = 'https://clipclap.co/paybutton/js/paybutton.min.js';
-        var s = document.getElementsByTagName('script')[0]; 
-        s.parentNode.insertBefore(cc, s);
+(function(){
+    console.log(window._$clipclap);
+    // console.log('call button file');
+    var cc = document.createElement('script'); cc.type = 'text/javascript'; cc.async = true;
+    cc.src = 'https://clipclap.co/paybutton/js/paybutton.min.js';
+    var s = document.getElementsByTagName('script')[0]; 
+    s.parentNode.insertBefore(cc, s);
+})()
+        
 
 /*global define*/
 define(
