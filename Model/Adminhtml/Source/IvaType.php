b64c95d9abeb1fd2820e7f22eb6b1099661072f0
Jairo Mejia <jairo@codeiseverywhere.com>
2016-03-12 12:17:06 -0500

    update

--- Model/Adminhtml/Source/IvaType.php
+++ Model/Adminhtml/Source/IvaType.php
@@ -18,13 +18,13 @@ class IvaType implements \Magento\Framework\Option\ArrayInterface
     public function toOptionArray()
     {
         return [
-            'value' => '16',  'label' => 'IVA Regular del 16%', 
-            'value' => '5',  'label' => 'IVA Reducido del 5%', 
-            'value' => '0',  'label' => 'IVA Excento del 0%', 
-            'value' => '0',  'label' => 'IVA Excluído del 0%', 
-            'value' => '8',  'label' => 'Consumo Regular 8%', 
-            'value' => '4',  'label' => 'Consumo Reducido 4%', 
-            'value' => '20',  'label' => 'IVA Ampliado 20%']
+            ['value' => '16',  'label' => 'IVA Regular del 16%'], 
+            ['value' => '5',  'label' => 'IVA Reducido del 5%'], 
+            ['value' => '0',  'label' => 'IVA Excento del 0%'], 
+            ['value' => '0',  'label' => 'IVA Excluído del 0%'], 
+            ['value' => '8',  'label' => 'Consumo Regular 8%'], 
+            ['value' => '4',  'label' => 'Consumo Reducido 4%'], 
+            ['value' => '20',  'label' => 'IVA Ampliado 20%']
         ];
     }
 }
