0570706f825d65c4cbf16b3618951ea6c9e07cbb
Jairo Mejia <jairo@codeiseverywhere.com>
2016-03-19 22:46:42 -0500

    update

--- Model/Ui/ConfigProvider.php
+++ Model/Ui/ConfigProvider.php
@@ -6,226 +6,47 @@
 namespace Magento\ClipClapGateway\Model\Ui;
 
 use Magento\Checkout\Model\ConfigProviderInterface;
-// use Magento\ClipClapGateway\Gateway\Http\Client\ClientMock;
+use Magento\ClipClapGateway\Gateway\Http\Client\ClientMock;
+use Magento\Checkout\Model\Session as CheckoutSession;
+
 
 /**
  * Class ConfigProvider
  */
 
-use Magento\Checkout\Helper\Data as CheckoutHelper;
-use Magento\Checkout\Model\Session as CheckoutSession;
-use Magento\Customer\Api\CustomerRepositoryInterface as CustomerRepository;
-use Magento\Customer\Model\Context as CustomerContext;
-use Magento\Customer\Model\Registration as CustomerRegistration;
-use Magento\Customer\Model\Session as CustomerSession;
-use Magento\Customer\Model\Url as CustomerUrlManager;
-use Magento\Framework\App\Http\Context as HttpContext;
-use Magento\Framework\Data\Form\FormKey;
-use Magento\Framework\Locale\CurrencyInterface as CurrencyManager;
-use Magento\Quote\Api\CartRepositoryInterface;
-use Magento\Quote\Api\CartItemRepositoryInterface as QuoteItemRepository;
-use Magento\Quote\Api\ShippingMethodManagementInterface as ShippingMethodManager;
-use Magento\Catalog\Helper\Product\ConfigurationPool;
-use Magento\Quote\Model\QuoteIdMaskFactory;
-use Magento\Framework\Locale\FormatInterface as LocaleFormat;
-use Magento\Framework\UrlInterface;
-use Magento\Quote\Api\CartTotalRepositoryInterface;
-use Magento\Framework\App\Config\ScopeConfigInterface;
-use Magento\Store\Model\ScopeInterface;
-/**
- * @SuppressWarnings(PHPMD.CouplingBetweenObjects)
- * @SuppressWarnings(PHPMD.TooManyFields)
- */
 class ConfigProvider implements ConfigProviderInterface
 {
-    /**
-     * @var CheckoutHelper
-     */
-    private $checkoutHelper;
+    const CODE = 'clipclap_gateway';
+
     /**
      * @var CheckoutSession
      */
     private $checkoutSession;
-    /**
-     * @var CustomerRepository
-     */
-    private $customerRepository;
-    /**
-     * @var CustomerSession
-     */
-    private $customerSession;
-    /**
-     * @var CustomerUrlManager
-     */
-    private $customerUrlManager;
-    /**
-     * @var HttpContext
-     */
-    private $httpContext;
-    /**
-     * @var \Magento\Quote\Api\CartRepositoryInterface
-     */
-    private $quoteRepository;
-    /**
-     * @var QuoteItemRepository
-     */
-    private $quoteItemRepository;
-    /**
-     * @var ShippingMethodManager
-     */
-    private $shippingMethodManager;
-    /**
-     * @var ConfigurationPool
-     */
-    private $configurationPool;
-    /**
-     * @param QuoteIdMaskFactory
-     */
-    protected $quoteIdMaskFactory;
-    /**
-     * @var LocaleFormat
-     */
-    protected $localeFormat;
-    /**
-     * @var \Magento\Customer\Model\Address\Mapper
-     */
-    protected $addressMapper;
-    /**
-     * @var \Magento\Customer\Model\Address\Config
-     */
-    protected $addressConfig;
-    /**
-     * @var FormKey
-     */
-    protected $formKey;
-    /**
-     * @var \Magento\Catalog\Helper\Image
-     */
-    protected $imageHelper;
-    /**
-     * @var \Magento\Framework\View\ConfigInterface
-     */
-    protected $viewConfig;
-    /**
-     * @var \Magento\Directory\Model\Country\Postcode\ConfigInterface
-     */
-    protected $postCodesConfig;
-    /**
-     * @var \Magento\Directory\Helper\Data
-     */
-    protected $directoryHelper;
-    /**
-     * @var Cart\ImageProvider
-     */
-    protected $imageProvider;
-    /**
-     * @var CartTotalRepositoryInterface
-     */
-    protected $cartTotalRepository;
-    /**
-     * @var ScopeConfigInterface
-     */
-    protected $scopeConfig;
-    /**
-     * @var \Magento\Shipping\Model\Config
-     */
-    protected $shippingMethodConfig;
-    /**
-     * @var \Magento\Store\Model\StoreManagerInterface
-     */
-    protected $storeManager;
+
     /**
      * @var \Magento\Quote\Api\PaymentMethodManagementInterface
      */
     protected $paymentMethodManagement;
-    /**
-     * @var UrlInterface
-     */
-    protected $urlBuilder;
-    /**
-     * @param CheckoutHelper $checkoutHelper
-     * @param Session $checkoutSession
-     * @param CustomerRepository $customerRepository
-     * @param CustomerSession $customerSession
-     * @param CustomerUrlManager $customerUrlManager
-     * @param HttpContext $httpContext
-     * @param \Magento\Quote\Api\CartRepositoryInterface $quoteRepository
-     * @param QuoteItemRepository $quoteItemRepository
-     * @param ShippingMethodManager $shippingMethodManager
-     * @param ConfigurationPool $configurationPool
-     * @param QuoteIdMaskFactory $quoteIdMaskFactory
-     * @param LocaleFormat $localeFormat
-     * @param \Magento\Customer\Model\Address\Mapper $addressMapper
-     * @param \Magento\Customer\Model\Address\Config $addressConfig
-     * @param FormKey $formKey
-     * @param \Magento\Catalog\Helper\Image $imageHelper
-     * @param \Magento\Framework\View\ConfigInterface $viewConfig
-     * @param \Magento\Directory\Model\Country\Postcode\ConfigInterface $postCodesConfig
-     * @param Cart\ImageProvider $imageProvider
-     * @param \Magento\Directory\Helper\Data $directoryHelper
-     * @param CartTotalRepositoryInterface $cartTotalRepository
-     * @param ScopeConfigInterface $scopeConfig
-     * @param \Magento\Shipping\Model\Config $shippingMethodConfig
-     * @param \Magento\Store\Model\StoreManagerInterface $storeManager
-     * @param \Magento\Quote\Api\PaymentMethodManagementInterface $paymentMethodManagement
-     * @param UrlInterface $urlBuilder
-     * @codeCoverageIgnore
-     * @SuppressWarnings(PHPMD.ExcessiveParameterList)
-     */
+
     public function __construct(
-        CheckoutHelper $checkoutHelper,
         CheckoutSession $checkoutSession,
-        CustomerRepository $customerRepository,
-        CustomerSession $customerSession,
-        CustomerUrlManager $customerUrlManager,
-        HttpContext $httpContext,
-        \Magento\Quote\Api\CartRepositoryInterface $quoteRepository,
-        QuoteItemRepository $quoteItemRepository,
-        ShippingMethodManager $shippingMethodManager,
-        ConfigurationPool $configurationPool,
-        QuoteIdMaskFactory $quoteIdMaskFactory,
-        LocaleFormat $localeFormat,
-        \Magento\Customer\Model\Address\Mapper $addressMapper,
-        \Magento\Customer\Model\Address\Config $addressConfig,
-        FormKey $formKey,
-        \Magento\Catalog\Helper\Image $imageHelper,
-        \Magento\Framework\View\ConfigInterface $viewConfig,
-        \Magento\Directory\Model\Country\Postcode\ConfigInterface $postCodesConfig,
-        Cart\ImageProvider $imageProvider,
-        \Magento\Directory\Helper\Data $directoryHelper,
-        CartTotalRepositoryInterface $cartTotalRepository,
-        ScopeConfigInterface $scopeConfig,
-        \Magento\Shipping\Model\Config $shippingMethodConfig,
-        \Magento\Store\Model\StoreManagerInterface $storeManager,
         \Magento\Quote\Api\PaymentMethodManagementInterface $paymentMethodManagement,
-        UrlInterface $urlBuilder
+        array $data = array()
     ) {
-        $this->checkoutHelper = $checkoutHelper;
+        // parent::__construct($context,
+        //     $registry,
+        //     $extensionFactory,
+        //     $customAttributeFactory,
+        //     $paymentData,
+        //     $scopeConfig,
+        //     $data);
+ 
+        // $this->_scopeConfig = $scopeConfig;
+        // // $this->_merchantKey =  $this->getConfigData('merchant_key');
+        // $this->_merchantKey = $this->_scopeConfig->getValue('merchant_id');
+        // $this->getConfigData('merchant_key')
         $this->checkoutSession = $checkoutSession;
-        $this->customerRepository = $customerRepository;
-        $this->customerSession = $customerSession;
-        $this->customerUrlManager = $customerUrlManager;
-        $this->httpContext = $httpContext;
-        $this->quoteRepository = $quoteRepository;
-        $this->quoteItemRepository = $quoteItemRepository;
-        $this->shippingMethodManager = $shippingMethodManager;
-        $this->configurationPool = $configurationPool;
-        $this->quoteIdMaskFactory = $quoteIdMaskFactory;
-        $this->localeFormat = $localeFormat;
-        $this->addressMapper = $addressMapper;
-        $this->addressConfig = $addressConfig;
-        $this->formKey = $formKey;
-        $this->imageHelper = $imageHelper;
-        $this->viewConfig = $viewConfig;
-        $this->postCodesConfig = $postCodesConfig;
-        $this->imageProvider = $imageProvider;
-        $this->directoryHelper = $directoryHelper;
-        $this->cartTotalRepository = $cartTotalRepository;
-        $this->scopeConfig = $scopeConfig;
-        $this->shippingMethodConfig = $shippingMethodConfig;
-        $this->storeManager = $storeManager;
         $this->paymentMethodManagement = $paymentMethodManagement;
-        $this->urlBuilder = $urlBuilder;
     }
 
     public function getConfig()
