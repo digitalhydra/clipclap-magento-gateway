353d80acc8ffe3f3180b48afd04f75b580fbde2e
Jairo Mejia <jairo@codeiseverywhere.com>
2016-03-19 23:06:02 -0500

    update

--- Model/Ui/ConfigProvider.php
+++ Model/Ui/ConfigProvider.php
@@ -7,59 +7,56 @@ namespace Magento\ClipClapGateway\Model\Ui;
 
 use Magento\Checkout\Model\ConfigProviderInterface;
 use Magento\ClipClapGateway\Gateway\Http\Client\ClientMock;
-use Magento\Checkout\Model\Session as CheckoutSession;
-use Magento\Customer\Model\Session as CustomerSession;
 
+use Magento\Quote\Api\Data\CartInterface;
+use Magento\Payment\Model\Method\AbstractMethod;
+use Magento\TMRobokassa\Model\Config\Source\Order\Status\Paymentreview;
+use Magento\Sales\Model\Order;
 
 /**
  * Class ConfigProvider
  */
-
-class ConfigProvider implements ConfigProviderInterface
+final class ConfigProvider implements ConfigProviderInterface
 {
     const CODE = 'clipclap_gateway';
 
-    /**
-     * @var CheckoutSession
-     */
-    private $checkoutSession;
-
-    private $customerSession;
-    /**
-     * @var \Magento\Quote\Api\PaymentMethodManagementInterface
-     */
-    protected $paymentMethodManagement;
-
     public function __construct(
-        CheckoutSession $checkoutSession,
-        CustomerSession $customerSession,
-        \Magento\Quote\Api\PaymentMethodManagementInterface $paymentMethodManagement,
+        \Magento\Framework\Model\Context $context,
+        \Magento\Framework\Registry $registry,
+        \Magento\Framework\Api\ExtensionAttributesFactory $extensionFactory,
+        \Magento\Framework\Api\AttributeValueFactory $customAttributeFactory,
+        \Magento\Payment\Helper\Data $paymentData,
+        \Magento\Framework\App\Config\ScopeConfigInterface $scopeConfig,
+        \Magento\Payment\Model\Method\Logger $logger,
+        \Magento\Framework\Module\ModuleListInterface $moduleList,
+        \Magento\Framework\Stdlib\DateTime\TimezoneInterface $localeDate,
+        \Magento\Directory\Model\CountryFactory $countryFactory,
+        \Stripe\Stripe $stripe,
         array $data = array()
     ) {
-        
-        // parent::__construct($context,
-        //     $registry,
-        //     $extensionFactory,
-        //     $customAttributeFactory,
-        //     $paymentData,
-        //     $scopeConfig,
-        //     $data);
- 
-        // $this->_scopeConfig = $scopeConfig;
-        // // $this->_merchantKey =  $this->getConfigData('merchant_key');
-        // $this->_merchantKey = $this->_scopeConfig->getValue('merchant_id');
-        // $this->getConfigData('merchant_key')
-        $this->checkoutSession = $checkoutSession;
-        $this->customerSession = $customerSession;
-        $quoteId = $this->checkoutSession->getQuote()->getId();
-        $this->paymentMethodManagement = $paymentMethodManagement;
+        parent::__construct(
+            $context,
+            $registry,
+            $extensionFactory,
+            $customAttributeFactory,
+            $paymentData,
+            $scopeConfig,
+            $logger,
+            $moduleList,
+            $localeDate,
+            null,
+            null,
+            $data
+        );
     }
 
+    /**
+     * Retrieve assoc array of checkout configuration
+     *
+     * @return array
+     */
     public function getConfig()
     {
-
-        // $output['paymentMethods'] = $this->getPaymentMethods();
-
         return [
             'payment' => [
                 self::CODE => [
@@ -67,31 +64,11 @@ class ConfigProvider implements ConfigProviderInterface
                         ClientMock::SUCCESS => __('Success'),
                         ClientMock::FAILURE => __('Fraud')
                     ],
-                    'merchantKey' =>  $this->_merchantKey,
+                    'merchantKey' => 'some_key',
                     'buttonTheme' => 'el azul',
                     'ivaTax' => 'el iva',
                 ]
-            ],
-            'paymentMethods'=>$this->getPaymentMethods()
+            ]
         ];
-    }
-
-    /**
-     * Returns array of payment methods
-     * @return array
-     */
-    private function getPaymentMethods()
-    {
-        $paymentMethods = [];
-        $quote = $this->checkoutSession->getQuote();
-        if ($quote->getIsVirtual()) {
-            foreach ($this->paymentMethodManagement->getList($quote->getId()) as $paymentMethod) {
-                $paymentMethods[] = [
-                    'code' => $paymentMethod->getCode(),
-                    'title' => $paymentMethod->getTitle()
-                ];
-            }
-        }
-        return $paymentMethods;
     }
 }
