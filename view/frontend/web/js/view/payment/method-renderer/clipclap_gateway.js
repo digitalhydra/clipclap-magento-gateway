8162e0544de0c16be77e05a65139a41a3eb08e8e
Jairo Mejia <jairo@codeiseverywhere.com>
2016-03-21 16:53:51 -0500

    update

--- view/frontend/web/js/view/payment/method-renderer/clipclap_gateway.js
+++ view/frontend/web/js/view/payment/method-renderer/clipclap_gateway.js
@@ -65,6 +65,32 @@ define(
                     evt.initEvent('load',false,false);
                     window.dispatchEvent(evt);
 
+                    _$clipclap.transactionState = this.getTransactionState();
+
+            },
+            getTransactionState:function(status, codRespuesta, paymentRef, token, numAprobacion, fechaTransaccion){
+
+                    // jQuery("body").block({
+                    //     message: "Estamos procesando el pedido.",
+                    //     baseZ: 99999,
+                    //     overlayCSS:{ background: "#fff", opacity: 0.6 },
+                    //     css: { padding:"20px", zindex:"9999999", textAlign:"center",color:"#555",border: "3px solid #aaa",backgroundColor:"#fff",cursor:"wait",lineHeight:"24px",
+                    //     }
+                    // });
+                    // var transactionData = {
+                    //     'estado' : status,
+                    //     'codRespuesta' : codRespuesta,
+                    //     'paymentRef' : '<?php echo $clipclap_args["order_id"]; ?>',
+                    //     'token' : token,
+                    //     'numAprobacion' : numAprobacion,
+                    //     'fechaTransaccion' : fechaTransaccion
+                    // }
+                    
+                    // jQuery.post( '<?php echo $api_url; ?>', transactionData )
+                    // .done(function( data ) {
+                    //     window.location = '<?php echo $redirect_url; ?>';
+                    // });
+
             },
             getCode: function() {
                 // console.log('getCode');
